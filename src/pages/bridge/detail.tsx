4363e0aa8c513aab28ad4d1262232c7cfa6eedba
ibwei <bawei3600@gmail.com>
2021-07-15 15:10:49 +0800

    fix(bridge-detail): update text

--- src/pages/bridge/detail.tsx
+++ src/pages/bridge/detail.tsx
@@ -336,7 +336,7 @@ const BridgeDetailPage: React.FunctionComponent<BridgeDetailPageProps> = (props)
                 <StatusText style={{ color: '#31D7A0' }}>{t(`SUCCESS`)}!</StatusText>
               </>
             ) : (
-              <StatusText>{t(`${order?.status}`)}</StatusText>
+              <StatusText>{t(`${order?.id ? order?.status : 'Process'}`)}</StatusText>
             )}
           </CenterRow>
         </StepsWrap>
