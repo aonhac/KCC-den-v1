93446b2ccf07448e96445d7257137a2eace0634f
ibwei <bawei3600@gmail.com>
2021-07-15 11:46:29 +0800

    fix(test): for test

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -66,7 +66,8 @@ const TEST_NETWORKS = {
   },
 }
 
-export const networks = process.env.NODE_ENV === 'production' ? MAIN_NETWORKS : { ...MAIN_NETWORKS, ...TEST_NETWORKS }
+// export const networks = process.env.NODE_ENV === 'production' ? MAIN_NETWORKS : { ...MAIN_NETWORKS, ...TEST_NETWORKS }
+export const networks = { ...MAIN_NETWORKS, ...TEST_NETWORKS }
 
 export const KCC_NETWORK_IDS = [322, 321]
 
