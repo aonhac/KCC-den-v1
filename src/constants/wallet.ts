c676e30c98387bc64bd29ece4da65c8a91e8d4be
ibaiwei <bawei3600@gmail.com>
2021-07-29 22:37:26 +0800

    fix(format): format

--- src/constants/wallet.ts
+++ src/constants/wallet.ts
@@ -3,14 +3,14 @@ export const OppositeNetworkName = 'OPPSITE'
 export const connectorLocalStorageKey = 'connectorId'
 
 export enum ConnectorNames {
-    Injected = 'injected',
-    WalletConnect = 'walletconnect',
+  Injected = 'injected',
+  WalletConnect = 'walletconnect',
 }
 
 export const WalletList: any[] = [
-    {
-        id: 0,
-        name: 'MetaMask',
-        logo: require('../assets/images/bridge/selected-bg.png').default,
-    },
+  {
+    id: 0,
+    name: 'MetaMask',
+    logo: require('../assets/images/bridge/selected-bg.png').default,
+  },
 ]
